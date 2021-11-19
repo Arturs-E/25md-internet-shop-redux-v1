6ab19b21932167f922d5e6e7acb3bf41c160d7d1
Arturs Endzelins <arturs.endzelins@gmail.com>
2021-11-19 15:45:41 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.scss';
+import Header from './components/Header/Header';
+
+const App = () => (
+  <div className="internet-shop">
+    <div className="internet-shop-wrapper">
+      <Header />
+      <p>Test</p>
+    </div>
+  </div>
+);
+
+export default App;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Header.scss';
+
+const Header = () => (
+  <div className="header">
+    <img src="./assets/logo/Leica_Geosystems_Logo.svg" alt="Leica Geosystems logo" className="logo" />
+  </div>
+);
+
+export default Header;
