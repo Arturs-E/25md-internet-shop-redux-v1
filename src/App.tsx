6583586d55c76480940315ac6f416e9f3bf687bd
Arturs Endzelins <arturs.endzelins@gmail.com>
2022-01-04 10:56:31 +0200

    Updated README

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,25 @@
 import React from 'react';
 import './App.scss';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {
+  BrowserRouter, Routes, Route, HashRouter,
+} from 'react-router-dom';
 import Header from './components/Header/Header';
 import Home from './pages/Home';
 import ShoppingCart from './pages/ShoppingCart';
 
 const App = () => (
   <BrowserRouter>
-    <div className="internet-shop">
-      <div className="internet-shop-wrapper">
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/cart" element={<ShoppingCart />} />
-        </Routes>
+    <HashRouter basename="/">
+      <div className="internet-shop">
+        <div className="internet-shop-wrapper">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<ShoppingCart />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </HashRouter>
   </BrowserRouter>
 );
 
